fa6da702283a2845e019f914cef0ba4561c22a1a
Florian Franzen <FlorianFranzen@gmail.com>
2014-01-10 06:53:29 +0100

    Adds automatic download of podcast.xml. Also cleans up script and fixes two minor bugs. (Unespaced dots in regex and season = 30 in rename script.)

--- podcastXML2xbmcNFO.py
+++ podcastXML2xbmcNFO.py
@@ -7,12 +7,13 @@
 #
 # (c) 2014 by Florian Franzen
 
-# ToDo: Add curl to download xml.
 # ToDo: Scrap 'Fahrplan' info and add it to nfo (e.g. pictures of speaker)
+# ToDo: Allow diffrent ending. Right now only '_h264-hq.mp4' is supported
 
 import argparse
 
 import xml.etree.ElementTree as ET
+import urllib
 import re
 import codecs
 import os
@@ -20,54 +21,24 @@ from collections import namedtuple
 
 Talk = namedtuple("Talk", "title subtitle description speakers category prefix")
 
-
 parser = argparse.ArgumentParser(description='Creates NFO files for each entry in a podcast.xml file.')
-parser.add_argument('YEAR', type=int, help='The conference the podcast.xml is from e.g. 30 if you run this for the 30c3.')
-parser.add_argument('XML_FILE', help='Path to the xml file.')
+
+parser.add_argument('-x', '--xml-file', help='Path to the xml file. (Default: downloaded automatically)')
+
+parser.add_argument('CONF_NUM', type=int, help='The conference the podcast.xml is from e.g. 30 if you run this for the 30c3.')
 config = parser.parse_args()
 
-
-def createNFO(episode, id_num, talk, season, num_digit=2):
-    xml_file = r"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-    <episodedetails>
-    <showtitle>Chaos Communication Congress</showtitle>
-    <season>%d</season>
-    <episode>%d</episode>
-    <title>%s</title>""" % (season, episode, talk.title)
-
-    if talk.subtitle is not None:
-        xml_file += """
-    <outline>%s</outline>""" % talk.subtitle
-
-    xml_file += """
-    <year>%s</year>
-    <id>%s</id>
-    <plot>
-        %s
-    </plot>
-    <genre>Talk</genre>
-    <tag>%s</tag>
-    """ % (1983 + season, id_num, talk.description, talk.category)
-
-    for speaker in talk.speakers:
-        xml_file += r"""<actor>
-        <name>%s</name>
-        <role></role>
-    </actor>
-    """ % speaker.strip()
-
-    xml_file += r"</episodedetails>"
-
-    file_pointer = codecs.open(talk.prefix + "_h264-hq.s%02de%0*d.nfo" % (season, num_digit, episode), "w", "utf-8-sig")
-    file_pointer.write(xml_file)
-    file_pointer.close()
-
-# Parse XML file first
-tree = ET.parse(config.XML_FILE)
-root = tree.getroot()
+# Parse XML file 
+if config.xml_file is not None:
+    tree = ET.parse(config.xml_file)
+    root = tree.getroot()
+else:
+    podcast_xml = urllib.urlopen("http://media.ccc.de/browse/congress/%d/podcast.xml" % (1983 + config.CONF_NUM)).read()
+    root = ET.fromstring(podcast_xml)
 
 # Make sure directory exist
-directory = 'S%02d/' % config.YEAR
+directory = 'S%02d/' % config.CONF_NUM
+
 if not os.path.exists(directory):
     os.makedirs(directory)
 os.chdir(directory)
@@ -84,18 +55,23 @@ for item in root[0].findall('item'):
 
     category = item.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}keywords').text
 
-    speakers = item.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}author').text.split(",")
+    speakers = item.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}author')
+    if speakers is not None:
+        speakers = speakers.text.split(",")
+    else:
+        speakers = []
 
     description = item.find('description').text
     description = re.split('event on media: |about this event: ', description)[0].strip()
 
     # Extract prefix from link
     link = item.find('link').text
-    match = re.search(r'(?<=/)[^/]*(?=(_webm.webm|_h264(-hq|-iprod)?.mp4))', link)
+    match = re.search(r'(?<=/)[^/]*(?=(_webm\.webm|_h264(-hq|-iprod)?\.mp4))', link)
     if match is not None:
         prefix = match.group(0)
     else:
         print("Can not extract prefix from following link: ", link)
+        continue
 
     # Extract id from prefix
     match = re.search(r'(?<=\d{2}c3-)\d{4}(?=-)', prefix)
@@ -103,6 +79,7 @@ for item in root[0].findall('item'):
         id_num = int(match.group(0))
     else:
         print("Can not extract ID from prefix: ", prefix)
+        continue
 
     all_talks[id_num] = Talk(title, subtitle, description, speakers, category, prefix)
 
@@ -120,10 +97,41 @@ for id_num in sorted(all_talks):
     talk = all_talks[id_num]
 
     # ... create NFO files
-    createNFO(episode, id_num, talk, config.YEAR, num_digit)
+    xml_file = r"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+    <episodedetails>
+        <showtitle>Chaos Communication Congress</showtitle>
+        <season>%d</season>
+        <episode>%d</episode>
+        <title>%s</title>""" % (config.CONF_NUM, episode, talk.title)
+
+    if talk.subtitle is not None:
+        xml_file += """
+        <outline>%s</outline>""" % talk.subtitle
+
+    xml_file += """
+        <year>%s</year>
+        <id>%s</id>
+        <plot>
+            %s
+        </plot>
+        <genre>Talk</genre>
+        <tag>%s</tag>
+    """ % (1983 + config.CONF_NUM, id_num, talk.description, talk.category)
+
+    for speaker in talk.speakers:
+        xml_file += r"""    <actor>
+            <name>%s</name>
+            <role></role>
+        </actor>
+    """ % speaker.strip()
+
+    xml_file += r"</episodedetails>"
+
+    file_pointer = codecs.open(talk.prefix + "_h264-hq.s%02de%0*d.nfo" % (config.CONF_NUM, num_digit, episode), "w", "utf-8-sig")
+    file_pointer.write(xml_file)
 
     # ... collect the file names for the renaming script
-    rename_file += "mv '%s' '%s'\n" % (talk.prefix + "_h264-hq.mp4", talk.prefix + "_h264-hq.s30e%0*d.mp4" % (num_digit, episode))
+    rename_file += "mv '%s' '%s'\n" % (talk.prefix + "_h264-hq.mp4", talk.prefix + "_h264-hq.s%02de%0*d.mp4" % (config.CONF_NUM, num_digit, episode))
 
     episode += 1
 
@@ -131,3 +139,5 @@ for id_num in sorted(all_talks):
 f = open("rename.sh", "w")
 f.write(rename_file)
 f.close()
+
+print("Finished parsing all %d talks of %dc3." % (episode - 1, config.CONF_NUM))
